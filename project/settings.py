d18307e5787d6577f1569334343f40c03a7aea95
Magdeldinr <magdeldinr@gmail.com>
2022-04-15 19:12:45 +0200

    adding docker file and requirements.txt

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%cqhdo5^1eqm-((9zkl$%ew+si)17ejwliiz45sydu3ms4+=%2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
